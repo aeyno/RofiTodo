b1d69d76938f4b6001c91259f970394dcf939fc5
Tom Gouville <tom.gouville@protonmail.com>
2021-10-15 08:44:53 +0200

    [DOC] Adding comments to Indexer type

--- src/indexer.rs
+++ src/indexer.rs
@@ -5,6 +5,7 @@ use std::cmp::Ordering;
 
 type CompareFunction<T> = fn(&T,&T) -> Ordering;
 
+/// Store data data with mutiple indexes and filters
 pub struct Indexer<T> {
     /// An index which stores all the values without filtering
     main_index : BTreeSet<Rc<T>>,
@@ -13,10 +14,17 @@ pub struct Indexer<T> {
 }
 
 impl<T : std::cmp::Ord> Indexer<T> {
+
+    /// Create new Indexer
     pub fn new() -> Self {
         Indexer { main_index : BTreeSet::<Rc<T>>::new() , indexes : HashMap::<String,Index<T>>::new() }
     }
 
+    /// Index a new element into the structure
+    /// 
+    /// Arguments:
+    /// 
+    /// * `element` - the element to index
     pub fn add(&mut self, element : T) -> Rc<T> {
         let e = Rc::new(element);
         self.main_index.insert(Rc::clone(&e));
@@ -26,10 +34,11 @@ impl<T : std::cmp::Ord> Indexer<T> {
         e
     }
 
-    pub fn get_main_index(&self) -> &BTreeSet<Rc<T>> {
-        &self.main_index
-    }
-
+    /// Remove an element from the structure
+    /// 
+    /// Arguments:
+    /// 
+    /// * `element` - Rc reference to the element to remove
     pub fn remove(&mut self, element : Rc<T>) -> Option<T> {
         self.main_index.remove(&element);
         let mut empty_indexes = Vec::<String>::new();
@@ -48,6 +57,19 @@ impl<T : std::cmp::Ord> Indexer<T> {
         }
     }
 
+    /// Get the main index to iterate over all the stored elements
+    pub fn get_main_index(&self) -> &BTreeSet<Rc<T>> {
+        &self.main_index
+    }
+
+    /// Create a new index for the data
+    /// Sort all the existing data at the creation of the index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `name` - the name of the new index
+    /// * `filter` - a closure to filter the elements (returns `true` if the value should be in the index)
+    /// * `compare_fn` - a function to compare and sort elements
     pub fn new_index(&mut self, name : String, filter : impl Fn(&T) -> bool + 'static, compare_fn : CompareFunction<T>) {
         match self.get_index(&name) {
             None => {
@@ -61,10 +83,20 @@ impl<T : std::cmp::Ord> Indexer<T> {
         }
     }
 
+    /// Remove an index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `name` - the name of the index
     pub fn remove_index(&mut self, name: &String) -> () {
         self.indexes.remove(name);
     }
 
+    /// Return a BTreeSet with the content of an Index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `name` - the name of the index
     pub fn get_index(&self, name : &String) -> Option<&BTreeSet<ElementWrapper<T>>> {
         match self.indexes.get(name) {
             Some(i) => Some(&i.content),
@@ -72,27 +104,58 @@ impl<T : std::cmp::Ord> Indexer<T> {
         }
     }
 
+    /// Return an reference to an Index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `name` - the name of the index
     pub fn index(&self, name : &String) -> Option<&Index<T>> {
         self.indexes.get(name)
     }
 }
 
+/// Contains a sorted list of pointers to elements
 pub struct Index<T> {
+    /// The content of the Index
     content : BTreeSet<ElementWrapper<T>>,
-    is_indexable :  Box<dyn Fn(&T) -> bool>,
+    /// A closure to filter the elements (returns `true` if the value should be in the index)
+    is_indexable : Box<dyn Fn(&T) -> bool>,
+    /// A function to compare and sort elements
     compare : CompareFunction<T>,
+    /// Indicates whether the Index should be removed when empty
     remove_if_empty : bool
 }
 
 impl<T> Index<T> {
+    /// Create a new Index
+    /// the `is_indexable` function is used to filter the elements
+    /// the `compare` function is used to sort the task inside the index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `is_indexable` - a closure to filter the elements (returns `true` if the value should be in the index)
+    /// * `compare_fn` - a function to compare and sort elements
     pub fn new(is_indexable : impl Fn(&T) -> bool + 'static, compare : CompareFunction<T>) -> Self {
         Index { content : BTreeSet::<ElementWrapper<T>>::new(), is_indexable : Box::new(is_indexable), compare : compare , remove_if_empty : false }
     }
 
+    /// Create a new Index which is removed when empty
+    /// the `is_indexable` function is used to filter the elements
+    /// the `compare` function is used to sort the task inside the index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `is_indexable` - a closure to filter the elements (returns `true` if the value should be in the index)
+    /// * `compare_fn` - a function to compare and sort elements
     pub fn new_autoremove(is_indexable : impl Fn(&T) -> bool + 'static, compare : CompareFunction<T>) -> Self {
         Index { content : BTreeSet::<ElementWrapper<T>>::new(), is_indexable : Box::new(is_indexable), compare : compare , remove_if_empty : true }
     }
 
+    /// Register a new element in the Index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `element` - a boxed element
     pub fn register(&mut self, element : Rc<T>) -> () {
         if (self.is_indexable)(element.as_ref()) {
             let ew = ElementWrapper::new(element, self.compare);
@@ -100,21 +163,29 @@ impl<T> Index<T> {
         }
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.content.is_empty()
-    }
-
-    pub fn remove_if_empty(&self) -> bool {
-        self.remove_if_empty
-    }
-
+    /// Remove an element from the Index
+    /// 
+    /// Arguments:
+    /// 
+    /// * `element` - a reference to boxed element
     pub fn remove(&mut self, element : &Rc<T>) {
         if (self.is_indexable)(&element) {
             self.content.remove(&ElementWrapper::new(Rc::clone(element), self.compare));
         }
     }
+
+    /// Return true if the Index is empty
+    pub fn is_empty(&self) -> bool {
+        self.content.is_empty()
+    }
+
+    /// Return true if the Index should be removed when empty
+    pub fn remove_if_empty(&self) -> bool {
+        self.remove_if_empty
+    }
 }
 
+/// Create an object to iterate over an Index without consuming it's content
 impl<'a, T> IntoIterator for &'a Index<T> {
     type Item = Rc<T>;
     type IntoIter = IndexIntoIterator<'a,T>;
@@ -126,12 +197,16 @@ impl<'a, T> IntoIterator for &'a Index<T> {
     }
 }
 
+/// A struct to iterate over an Index without consuming it's content
 pub struct IndexIntoIterator<'a, T> {
     btree_ite : Box<std::collections::btree_set::Iter<'a, ElementWrapper<T>>>,
 }
 
+/// Implements the ability to iterate over IndexIntoIterator
 impl<'a,T> Iterator for IndexIntoIterator<'a, T> {
     type Item = Rc<T>;
+
+    /// Get the next element of the iterator
     fn next(&mut self) -> Option<Self::Item> {
         match self.btree_ite.next() {
             Some(elem) => Some(Rc::clone(&elem.content)),
@@ -140,33 +215,46 @@ impl<'a,T> Iterator for IndexIntoIterator<'a, T> {
     }
 }
 
+/// An element of the Index which wrap the element to allow sorting
 pub struct ElementWrapper<T> {
+    /// A smart pointer to the element
     content : Rc<T>,
+    /// A function to compare two elements
     compare : CompareFunction<T>
 }
 
 impl<T> ElementWrapper<T> {
+    /// Create a new ElementWrapper for an element
+    /// 
+    /// Arguments:
+    /// 
+    /// * `elem` - a smart pointer to an element
+    /// * `compare_fn` - a function to compare and sort elements
     fn new(elem : Rc<T>, compare_fn : CompareFunction<T>) -> Self {
         ElementWrapper { content : elem, compare : compare_fn }
     }
 
+    /// Return a reference to the content
     pub fn _content(&self) -> &T {
         self.content.as_ref()
     }
 }
 
+/// Implementing ordering for `ElementWrapper` to allow BTreeSet to sort it
 impl<T> Ord for ElementWrapper<T> {
     fn cmp(&self, other: &Self) -> Ordering {
         (self.compare)(self.content.as_ref(), other.content.as_ref())
     }
 }
 
+/// Implementing `PartialOrd` to implement `Ord` for `ElementWrapper`
 impl<T> PartialOrd for ElementWrapper<T> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some((self.compare)(self.content.as_ref(), other.content.as_ref()))
     }
 }
 
+/// Implementing `PartialEq` to implement `PartialOrd` for `ElementWrapper`
 impl<T> PartialEq for ElementWrapper<T> {
     fn eq(&self, other: &Self) -> bool {
         if Rc::ptr_eq(&self.content, &other.content) { return true }
@@ -174,6 +262,7 @@ impl<T> PartialEq for ElementWrapper<T> {
     }
 }
 
+/// Implementing `Eq` to implement `PartialEq` for `ElementWrapper`
 impl<T> Eq for ElementWrapper<T> { }
 
 #[cfg(test)]
